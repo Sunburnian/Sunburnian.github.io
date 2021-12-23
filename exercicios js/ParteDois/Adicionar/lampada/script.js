5070e887f46e8ac35d355c21ed6d290b8820e9b0
Sunburnian <coonta.jogos.14@gmail.com>
2021-12-22 21:43:03 -0300

    lampada

---
+++ exercicios js/ParteDois/Adicionar/lampada/script.js
@@ -0,0 +1,12 @@
+function ligar(){
+    var aux = document.getElementById('lampada');
+    
+    aux.src='img/lampada_on.gif'
+    
+}
+
+function desligar(){
+    var aux2 = document.getElementById('lampada');
+
+    aux2.src = 'img/lampada_off.gif'
+}
