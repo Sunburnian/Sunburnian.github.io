042d28f8ef98b5fa4b4dcfefb07aa2da01784c2f
Sunburnian <coonta.jogos.14@gmail.com>
2021-12-22 21:29:01 -0300

    esconder/mostrar

---
+++ exercicios js/ParteDois/Adicionar/Ocultar/script.js
@@ -0,0 +1,13 @@
+function esconder(){
+
+    var aux = document.getElementById('teste');
+
+    aux = aux.style.display = "none";
+    
+}
+
+function mostrar(){
+    var aux2 = document.getElementById('teste');
+
+    aux2 = aux2.style.display = "block";
+}
